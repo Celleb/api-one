a227c8c030a5a651fa7bd4c5bd8db94ebee93d3c
Jonas Tomanga <tcelleb@gmail.com>
2018-01-28 13:25:07 +0200

    Update dictionary

--- src/core/types/dictionary.d.ts
+++ src/core/types/dictionary.d.ts
@@ -1,7 +1,4 @@
-export type NestedDictionary = {
-    _id: string;
-    [other: string]: string | NestedDictionary
-}
+
 export type Dictionary = {
-    [key: string]: string | NestedDictionary;
+    [key: string]: any;
 }
