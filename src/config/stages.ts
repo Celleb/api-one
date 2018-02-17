1e7e871d4f6406bc1a5968e8f2a849a279a57e24
Jonas Tomanga <tcelleb@gmail.com>
2018-02-17 16:58:54 +0200

    QueryBuilder stages

---
+++ src/config/stages.ts
@@ -0,0 +1,18 @@
+/**
+ * stages.ts
+ *
+ * @author Jonas Tomanga <celleb@mrcelleb.com>
+ * @copyright (c) 2018 Jonas Tomanga
+ * All rights reserved
+ * @license MIT
+ */
+
+export const stages = [
+    'exclude',
+    'include',
+    'limit',
+    'match',
+    'search',
+    'skip',
+    'sort'
+];
