1f7e1942ecc12112f9e92d114db42c199a2e22dc
Jonas Tomanga <tcelleb@gmail.com>
2018-03-07 18:27:32 +0200

    Added preWare and postWare options

--- src/core/types/route-config.d.ts
+++ src/core/types/route-config.d.ts
@@ -1,3 +1,14 @@
+/**
+ * route-config.d.ts
+ *
+ * @author Jonas Tomanga <celleb@mrcelleb.com>
+ * @copyright (c) 2018 Jonas Tomanga
+ * All rights reserved
+ * @license MIT
+ */
+
+import { Handler } from 'express';
+
 export interface RouteConfig {
     path: string;
     model?: string;
@@ -6,5 +17,19 @@ export interface RouteConfig {
     authorization?: {
         permissionID: string;
         permissions?: object;
+    },
+    preWare?: {
+        post?: Handler | Handler[],
+        get?: Handler | Handler[],
+        update?: Handler | Handler[],
+        delete?: Handler | Handler[],
+        patch?: Handler | Handler[]
+    },
+    postWare?: {
+        post?: Handler | Handler[],
+        get?: Handler | Handler[],
+        update?: Handler | Handler[],
+        delete?: Handler | Handler[],
+        patch?: Handler | Handler[]
     }
 }
